5ac7915ab20ff67e20e112840e0fdadb8fb857c7
dhj0057 <dhj00570@gmail.com>
2026-03-17 15:27:13 +0900

    add HTML5 template

---
+++ 03102/test.py
@@ -0,0 +1,27 @@
+
+me = {
+    "name" : "폴",
+    "age"  : 32,
+    "skills": ["python","HTML","CSS"]
+}
+me["age"] = 33
+me["skills"].append("javascript")
+print(f"나는 {me['skills'][0]}을 할줄안다.")
+            
+
+
+
+ 
+
+            
+       
+
+     
+          
+    
+    
+  
+        
+
+
+
